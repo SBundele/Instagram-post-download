c6112a417462a71bb65572e8912ed9aef688dcc3
Sourabh Bundele <sbundele0318@gmail.com>
2024-02-02 23:56:51 +0530

    added post download feature if account is not private

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import instaloader
 
 instagram = instaloader.Instaloader()
 username = input("Enter your username: ")
+
+profile = instaloader.Profile.from_username(instagram.context, username)
+
+private = profile.is_private
+
+if not private:
+    posts = list(profile.get_post())
+    latest_post = posts[0]
+    instagram.download_post(latest_post, target = profile.username)
+    
