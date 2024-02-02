fb10a6a0fb33fa49fd1a4d9fca1d6df1c1826af2
Sourabh Bundele <sbundele0318@gmail.com>
2024-02-02 23:59:29 +0530

    correct get_posts spelling

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ profile = instaloader.Profile.from_username(instagram.context, username)
 private = profile.is_private
 
 if not private:
-    posts = list(profile.get_post())
+    posts = list(profile.get_posts())
     latest_post = posts[0]
     instagram.download_post(latest_post, target = profile.username)
     
