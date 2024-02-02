1bb2dcedd9267bfcd33229a45b85274be29d9922
Sourabh Bundele <sbundele0318@gmail.com>
2024-02-03 00:16:42 +0530

    handled case where account is private with a message

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ username = input("Enter your username: ")
 profile = instaloader.Profile.from_username(instagram.context, username)
 
 private = profile.is_private
-
+    
 if not private:
     posts = list(profile.get_posts())
     latest_post = posts[0]
     instagram.download_post(latest_post, target = profile.username)
-    
+    print()
+    print("Thank you for using the App!!")
+else:
+    print("***Account is Private, Can't access posts or private data")
